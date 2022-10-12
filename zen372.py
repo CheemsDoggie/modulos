460426b0205f67f79efb5a80eff86ecfcd546147
Panchito <wolfogan@gmail.com>
2022-10-11 20:13:36 -0700

    Agregando base para modulos

---
+++ zen372.py
@@ -0,0 +1,16 @@
+# Comentario 1 linea
+
+"""
+Comentario multilinea
+1 Linea
+2 Lineas
+3 .....
+"""
+
+print("Latosa1: Nose")
+print("Latosa2: Mas vale pedir perdon que pedir permiso")
+print("Marco1: Camaron que se duerme bla bla bla")
+print("Marco2: Fuga!!!!")
+print("Derek: Fuga X 2")
+
+barba = "Algo que sorpresa!!!!"
